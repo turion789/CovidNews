101c99492c7ff55b7f2d904f5f11af3e9f1c57d3
turion789 <2103kbp@gmail.com>
2020-11-04 20:02:37 +0300

    Initial Commit

---
+++ CovidNews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CovidNews
+//
+//  Created by turion789 on 4.11.20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
